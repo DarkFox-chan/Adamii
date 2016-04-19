605a25d96b97a4806ec6289c94487edbe8864db8
Charles Muench <cbmuench@hotmail.com>
2016-04-19 11:11:33 -0700

    Fixed licenses and removed some unnecessary code from main.cpp

--- main.cpp
+++ main.cpp
@@ -7,16 +7,7 @@ void threadStage1()
 	Sprite *boss = createSprite(loadTexture("chaelin.png"), bossX, bossY, 0, 0, 64, 107);
 	Animation *bossAnim = new Animation(0, 0, 64, 107, 4, 8, MSPF(10), ANIMATION_LOOP);
 	Thread *bossAnimThread = animate(boss, bossAnim);
-	ShotType *BALL_RED = new ShotType(loadTexture("shots.png"), 32, 64, 32, 32, 8);
-	//Shot *shot = new Shot(AIM_CIRCLE, 100, 400, 300, 0.5, 0, BALL_RED, 30);
 	squirrelCall0(testVM, "main");
-	while (true)
-	{
-		//shot->fire();
-		//shot->angle += 30 * deltaTime;
-		//std::cout << "Core0: " << bullets0.size() << " Core1: " << bullets1.size() << " Total: " << (bullets0.size() + bullets1.size()) << " " << deltaTime << std::endl;
-		wait(3);
-	}
 }
 
 
